8a12c993f4306af2e22f3fdcb99103d80835a2fa
Julian Diaz <torresanibal336@gmail.com>
2025-11-06 14:11:25 -0500

    Se hace un cambio para que se entienda mas facil la funcion continue

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -4,9 +4,9 @@ for (let i = 1; i <= 10; i++) {
     let niño_con_dulce = prompt("El niño tiene dulce? si / no");
 
     if (niño_con_dulce == "si") {
-        console.log("Dulce entregado");
-    } else {
         console.log("No se entrega dulce");
         continue;
     }
+    
+    console.log("Dulce entregado");
 }
